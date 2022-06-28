67e9f5eb311757790cf6c28d5271de6122c0f054
Leonid Zaytsev <Leonid_Zaytsev@Dell.com>
2022-06-28 21:16:52 +0300

    Initial commit

---
+++ src/ru/tandemservice/test/task1/RowsComparator.java
@@ -0,0 +1,25 @@
+package ru.tandemservice.test.task1;
+
+import java.util.Comparator;
+
+public class RowsComparator implements Comparator<String[]> {
+    private final int columnIndex;
+    private NullSafeNumericalFirstComparator comparator;
+
+    public RowsComparator(final int columnIndex) {
+        this.columnIndex = columnIndex;
+        comparator = new NullSafeNumericalFirstComparator();
+    }
+
+    @Override
+    public int compare(final String[] row1, final String[] row2) {
+        try {
+            String cell_1 = row1[columnIndex];
+            String cell_2 = row2[columnIndex];
+            return comparator.compare(cell_1, cell_2);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.err.println("Rows can't be compared by column index: "+ columnIndex);
+            throw new IndexOutOfBoundsException(e.getMessage());
+        }
+    }
+}
---
+++ src/ru/tandemservice/test/task1/Task1.java
@@ -0,0 +1,7 @@
+package ru.tandemservice.test.task1;
+
+/*public class Task1 {
+    public static void main(String[] args) {
+
+    }
+}*/
